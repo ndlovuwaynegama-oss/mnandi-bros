a008471bf3ddcd24841a5c6b06032c2a3a13476b
ndlovuwaynegama-oss <ndlovuwaynegama@gmail.com>
2025-08-17 15:47:59 +0200

    Add files via upload

---
+++ Mnandi Bros Website/service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'mnandi-bros-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/mnandi-kota.jpg.jpg' // Add all your important assets here
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
